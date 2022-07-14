2a4550cb442402e7f778b8aaead14705bada5e0f
josemarza <josemarzamendia@gmail.com>
2022-07-14 18:45:45 -0400

    formulario de nuevos vehiculos

--- app/controller/main.py
+++ app/controller/main.py
@@ -5,3 +5,9 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@bp.route('/nuevovehiculo')
+def nuevovehiculo():
+    return render_template('nuevovehiculo.html')
+
