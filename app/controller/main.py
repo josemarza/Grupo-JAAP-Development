b95f9fcd1cec8e69dc1a92fe22eb23e5e5692084
josemarza <josemarzamendia@gmail.com>
2022-07-18 12:03:18 -0400

    Creacion de rutas y formularios de clientes y vehiculos

--- app/controller/main.py
+++ app/controller/main.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from datetime import datetime
+from app.model import Vehiculo
 
 bp = Blueprint('main', __name__)
 
@@ -11,3 +13,16 @@ def index():
 def nuevovehiculo():
     return render_template('nuevovehiculo.html')
 
+@bp.route('/nuevovehiculo2')
+def nuevovehiculo2():
+    return render_template('nuevovehiculo2.html')
+
+@bp.route('/api/nuevovehiculo',methods=['POST'])
+def apinuevovehiculo():
+    datos_vehiculo=request.form
+    print(datos_vehiculo)
+    fecha_actual=datetime.now()
+    
+    carga_de_vehiculo = Vehiculo.create(
+        vehic_chasis=datos_vehiculo['vehic_chasis'], vehic_marca=datos_vehiculo['vehic_marca'], vehic_modelo=datos_vehiculo['vehic_modelo'], vehic_anho=datos_vehiculo['vehic_anho'], vehic_tipo=datos_vehiculo['vehic_tipo'], vehic_color=datos_vehiculo['vehic_color'], vehic_combustible=datos_vehiculo['vehic_combustible'], vehic_kilometraje=datos_vehiculo['vehic_kilometraje'], vehic_estado=datos_vehiculo['vehic_estado'])
+    return render_template("nuevovehiculo.html")
